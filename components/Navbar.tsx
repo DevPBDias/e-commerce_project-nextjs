7114b180003e62ee02bb89bfc015aaf962a8a681
DevPBDias <devpbdias@gmail.com>
2024-04-08 18:42:08 -0300

    feat: creating navbar structure

---
+++ components/Navbar.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { links } from '../data/navbarLinks'
+import Link from 'next/link'
+
+const Navbar = () => {
+  return (
+    <header>
+        <div>
+            <p>Logo</p>
+        </div>
+        <div>
+            { links.map((item: any) => (
+                <Link key={item.id} href={item.path} >
+                    {item.name}
+                </Link>
+            ))}
+        </div>
+        <div>
+            <button type="button">Login</button>
+            <button type="button">Cart</button>
+        </div>
+    </header>
+  )
+}
+
+export default Navbar
---
+++ data/navbarLinks.ts
@@ -0,0 +1,22 @@
+export const links = [
+    {
+        id: 1,
+        name: 'Home',
+        path: '/',
+    },
+    {
+        id: 2,
+        name: 'Search',
+        path: '/search',
+    },
+    {
+        id: 3,
+        name: 'Our Story',
+        path: '/about',
+    },
+    {
+        id: 4,
+        name: 'Contact Us',
+        path: '/contact',
+    },
+]
